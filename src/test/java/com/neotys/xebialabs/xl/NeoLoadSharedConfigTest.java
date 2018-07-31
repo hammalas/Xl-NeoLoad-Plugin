1eeb2b4062498372c7121e0a04849eb2f62633aa
mlasram <mohamed.lasram@neotys.com>
2018-07-31 17:55:14 +0200

    add selenium tests last commit

--- src/test/java/com/neotys/xebialabs/xl/NeoLoadSharedConfigTest.java
+++ src/test/java/com/neotys/xebialabs/xl/NeoLoadSharedConfigTest.java
@@ -9,16 +9,27 @@ import org.testng.annotations.*;
 
 public class NeoLoadSharedConfigTest extends BaseTest {
 
-	public static final String SETTINGS = "Settings";
-	public static final String SHARED_CONFIGURATION = "Shared configuration";
-	WebDriverWait wait;
+	private static final String SETTINGS = "Settings";
+	private static final String SHARED_CONFIGURATION = "Shared configuration";
+	private static final String NEO_LOAD_CLOUD_CONFIGURATION_TEST = "NeoLoad Cloud Configuration Test";
+	private static final String NTS_COLLABORATION_TEST = "NTS Collaboration Test";
+	private static final String NEO_LOAD_WINDOWS = "NeoLoad Windows";
+	private static final String NEO_LOAD_TEAM_SERVER = "NeoLoad Team Server";
+	private static final String NEO_LOAD_WEB_PROD = "NeoLoad Web PROD";
+	private static final String NEO_LOAD_COLLABORATION = "NeoLoad Collaboration";
+	private static final String NEO_LOAD_CONTROLLER = "NeoLoad Controller";
+	private static final String NEO_LOAD_WEB = "NeoLoad Web";
+	private static final String NEO_LOAD_CLOUD = "NeoLoad Cloud";
+	private static final String NL_TEST = "NL test";
+	public static final String TEMPLATE_NEOLOAD_TEST = "Template Neoload Test";
+	private WebDriverWait wait;
 	private static final String login = "admin";
 	private static final String password = "Neotys13&#";
 
 	@BeforeClass
 	public void testLogin() {
 		System.out.println("called before class");
-		wait = new WebDriverWait(BaseTest.driver, 10L);
+		wait = new WebDriverWait(BaseTest.driver, 5L);
 		LoginPage.login(login, password);
 	}
 
@@ -26,11 +37,11 @@ public class NeoLoadSharedConfigTest extends BaseTest {
 	public void openConfigurationNeoloadCloud() {
 		MainMenu.clickMenu(SETTINGS);
 		SubMenu.clickSubMenu(SHARED_CONFIGURATION);
-		SharedConfigurationPage.openSharedConfiguration("NeoLoad Cloud");
-		SharedConfigurationPropertiesPage.checkSharedConfigurationHeader("Cloud");
-		SharedConfigurationPropertiesPage.setEditFieldBySequence(1, "NeoLoad Cloud Configuration Test");
-		SharedConfigurationPropertiesPage.setEditFieldBySequence(2, "admin");
-		SharedConfigurationPropertiesPage.setEditFieldBySequence(3, "admin");
+		SharedConfigurationPage.openSharedConfiguration(NEO_LOAD_CLOUD);
+		SharedConfigurationPropertiesPage.checkSharedConfigurationHeader(NEO_LOAD_CLOUD);
+		SharedConfigurationPropertiesPage.setEditFieldBySequence(1, NEO_LOAD_CLOUD_CONFIGURATION_TEST);
+		SharedConfigurationPropertiesPage.setEditFieldBySequence(2, login);
+		SharedConfigurationPropertiesPage.setEditFieldBySequence(3, password);
 		saveConfig();
 	}
 
@@ -38,12 +49,12 @@ public class NeoLoadSharedConfigTest extends BaseTest {
 	public void openConfigurationNeoloadCollaboration() {
 		MainMenu.clickMenu(SETTINGS);
 		SubMenu.clickSubMenu(SHARED_CONFIGURATION);
-		SharedConfigurationPage.openSharedConfiguration("NeoLoad Collaboration");
-		SharedConfigurationPropertiesPage.checkSharedConfigurationHeader("NeoLoad Collaboration");
-		SharedConfigurationPropertiesPage.setEditFieldBySequence(1, "NTS Collaboration Test");
+		SharedConfigurationPage.openSharedConfiguration(NEO_LOAD_COLLABORATION);
+		SharedConfigurationPropertiesPage.checkSharedConfigurationHeader(NEO_LOAD_COLLABORATION);
+		SharedConfigurationPropertiesPage.setEditFieldBySequence(1, NTS_COLLABORATION_TEST);
 		SharedConfigurationPropertiesPage.setEditFieldBySequence(2, "http://nts:8800/nts/svnroot/repository_1/");
-		SharedConfigurationPropertiesPage.setEditFieldBySequence(3, "admin");
-		SharedConfigurationPropertiesPage.setEditFieldBySequence(4, "admin");
+		SharedConfigurationPropertiesPage.setEditFieldBySequence(3, login);
+		SharedConfigurationPropertiesPage.setEditFieldBySequence(4, password);
 		saveConfig();
 	}
 
@@ -51,11 +62,9 @@ public class NeoLoadSharedConfigTest extends BaseTest {
 	public void openConfigurationNeoloadController() {
 		MainMenu.clickMenu(SETTINGS);
 		SubMenu.clickSubMenu(SHARED_CONFIGURATION);
-		SharedConfigurationPage.openSharedConfiguration("NeoLoad Controller");
-		SharedConfigurationPropertiesPage.checkSharedConfigurationHeader("NeoLoad Controller");
-		SharedConfigurationPropertiesPage.setEditFieldBySequence(1, "NeoLoad Windows");
-		//SharedConfigurationPropertiesPage.setOptionFromSelectFieldBySequence(1, "Windows - Telnet");
-		//SharedConfigurationPropertiesPage.setEditFieldBySequence(2, "localhost");
+		SharedConfigurationPage.openSharedConfiguration(NEO_LOAD_CONTROLLER);
+		SharedConfigurationPropertiesPage.checkSharedConfigurationHeader(NEO_LOAD_CONTROLLER);
+		SharedConfigurationPropertiesPage.setEditFieldBySequence(1, NEO_LOAD_WINDOWS);
 		SharedConfigurationPropertiesPage.setEditFieldBySequence(3, "path");
 		SharedConfigurationPropertiesPage.setEditFieldBySequence(4, login);
 		SharedConfigurationPropertiesPage.setEditFieldBySequence(5, password);
@@ -66,9 +75,9 @@ public class NeoLoadSharedConfigTest extends BaseTest {
 	public void openConfigurationNeoloadTeamServer() {
 		MainMenu.clickMenu(SETTINGS);
 		SubMenu.clickSubMenu(SHARED_CONFIGURATION);
-		SharedConfigurationPage.openSharedConfiguration("NeoLoad Team Server");
-		SharedConfigurationPropertiesPage.checkSharedConfigurationHeader("NeoLoad Team Server");
-		SharedConfigurationPropertiesPage.setEditFieldBySequence(1, "NeoLoad Team Server");
+		SharedConfigurationPage.openSharedConfiguration(NEO_LOAD_TEAM_SERVER);
+		SharedConfigurationPropertiesPage.checkSharedConfigurationHeader(NEO_LOAD_TEAM_SERVER);
+		SharedConfigurationPropertiesPage.setEditFieldBySequence(1, NEO_LOAD_TEAM_SERVER);
 		SharedConfigurationPropertiesPage.setEditFieldBySequence(2, "http://nts:9999/nts/");
 		SharedConfigurationPropertiesPage.setEditFieldBySequence(3, login);
 		SharedConfigurationPropertiesPage.setEditFieldBySequence(4, password);
@@ -80,9 +89,9 @@ public class NeoLoadSharedConfigTest extends BaseTest {
 	public void openConfigurationNeoloadWeb() {
 		MainMenu.clickMenu(SETTINGS);
 		SubMenu.clickSubMenu(SHARED_CONFIGURATION);
-		SharedConfigurationPage.openSharedConfiguration("NeoLoad Web");
-		SharedConfigurationPropertiesPage.checkSharedConfigurationHeader("NeoLoad Web");
-		SharedConfigurationPropertiesPage.setEditFieldBySequence(1, "NeoLoad Web PROD");
+		SharedConfigurationPage.openSharedConfiguration(NEO_LOAD_WEB);
+		SharedConfigurationPropertiesPage.checkSharedConfigurationHeader(NEO_LOAD_WEB);
+		SharedConfigurationPropertiesPage.setEditFieldBySequence(1, NEO_LOAD_WEB_PROD);
 		SharedConfigurationPropertiesPage.setEditFieldBySequence(2, "https://neoload.neotys.com");
 		saveConfig();
 	}
@@ -92,23 +101,29 @@ public class NeoLoadSharedConfigTest extends BaseTest {
 		MainMenu.clickMenu("Design");
 		SubMenu.clickSubMenu("Template");
 		TemplateListPage.clickNewTemplate();
-		CreateTemplatePage.createTemplateByName("test_NL");
+		CreateTemplatePage.createTemplateByName(TEMPLATE_NEOLOAD_TEST);
 		ReleasePage.newReleaseFromTemplate();
 		CreateReleasePage.createReleaseByName("Test NeoLoad");
-		ReleasePage.addTask("NL test", "Neo Load", "NeoLoad Test");
-		TaskDetailPage.selectItemByIndex(1, "NeoLoad Windows");
+		ReleasePage.addTask(NL_TEST, "Neo Load", "NeoLoad Test");
+		TaskDetailPage.selectItemByIndex(1, NEO_LOAD_WINDOWS);
+		TaskDetailPage.closeTaskDetails();
 	}
-
-//	@AfterMethod
-//	public void logout(){
-//		System.out.println("called after method");
-//		MainMenu.logout();
-//	}
 
 	@AfterClass
 	public void endSuite() {
-		deleteAllConfig();
 		MainMenu.logout();
+	}
+
+	private void deleteTemplate(final String templateName) {
+		MainMenu.clickMenu("Design");
+		SubMenu.clickSubMenu("Template");
+		BaseTest.driver.findElement(By.xpath("//strong[text()='" + templateName + "']/../../../span[text()='delete']")).click();
+	}
+
+	private void deleteTask(String taskName) {
+		BaseTest.driver.findElement(By.xpath("//span[text()='" + taskName + "']/../../..//i[@class='context-menu-icon']")).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='remove-task-button']/..")));
+		BaseTest.driver.findElement(By.xpath("//a[@class='remove-task-button']/..")).click();
 	}
 
 	private void saveConfig() {
@@ -123,11 +138,11 @@ public class NeoLoadSharedConfigTest extends BaseTest {
 	}
 
 	private void deleteAllConfig() {
-		deleteConfig("NeoLoad Cloud Configuration Test");
-		deleteConfig("NTS Collaboration Test");
-		deleteConfig("NeoLoad Windows");
-		deleteConfig("NeoLoad Team Server");
-		deleteConfig("NeoLoad Web PROD");
+		deleteConfig(NEO_LOAD_CLOUD_CONFIGURATION_TEST);
+		deleteConfig(NTS_COLLABORATION_TEST);
+		deleteConfig(NEO_LOAD_WINDOWS);
+		deleteConfig(NEO_LOAD_TEAM_SERVER);
+		deleteConfig(NEO_LOAD_WEB_PROD);
 	}
 
 
